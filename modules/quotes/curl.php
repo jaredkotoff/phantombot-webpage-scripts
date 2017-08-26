c3ae29481f276ae7be37e3c1e8a014b04a6b3d56
Jared Kotoff <jkotoff@veritone.com>
2017-08-26 13:19:18 -0700

    Upgrade quotes pages

--- modules/quotes/curl.php
+++ modules/quotes/curl.php
@@ -1,23 +1,12 @@
 <?php
     // Quotes
 		$name = "quotes";
-    curl_setopt($curl, CURLOPT_URL, "${protocol}://${url}/dbquery?table=quotes&getKeys");
+    curl_setopt($curl, CURLOPT_URL, "${protocol}://${url}/dbquery?table=quotes&getSortedRows&order=ASC");
     $results = json_decode(curl_exec($curl));
-    $quotes = [];
-
-    foreach ($results->table->keylist as $keys) {
-        $key = $keys->key;
-        curl_setopt($curl, CURLOPT_URL, "${protocol}://${url}/dbquery?table=quotes&getData=${key}");
-        $object = json_decode(curl_exec($curl));
-
-        $quotes[$key] = $object->table->value;
-    }
-    unset($results);
-    krsort($quotes);
-    $quotes = json_encode($quotes);
+    $results = json_encode($results);
 
     // write to a file
     $fp = fopen("${outputFolder}${name}.json", 'w');
-    fwrite($fp, $quotes);
+    fwrite($fp, $results);
     fclose($fp);
-    unset($quotes);
+    unset($results);
